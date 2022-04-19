808a76fadf792a22c1b005412c6275141aa608b9
dongchangbao <dongchangbao@papegames.net>
2022-04-19 20:21:34 +0800

    common update

--- script/python/lxutil_gui/.test/_tst__shot_render_submitter.py
+++ script/python/lxutil_gui/.test/_tst__shot_render_submitter.py
@@ -8,8 +8,8 @@ import lxutil_gui.panel.utl_pnl_widgets as utl_pnl_widgets
 bsc_core.EnvironMtd.set_td_enable(True)
 
 # hook_option = 'file={}'.format('/l/prod/cgm/work/shots/z88/z88070/ani/animation/maya/scenes/z88070.ani.animation.v026.ma')
-# hook_option = 'file={}'.format('/l/prod/xkt/work/shots/z88/z88010/efx/effects/houdini/z88010.efx.effects.v032.hip')
-hook_option = 'file={}'.format('/l/prod/xkt/work/shots/z88/z88100/cfx/hair/houdini/z88100.cfx.hair.v010.hip')
+hook_option = 'file={}'.format('/l/prod/xkt/work/shots/z88/z88010/efx/effects/houdini/z88010.efx.effects.v032.hip')
+# hook_option = 'file={}'.format('/l/prod/xkt/work/shots/z88/z88100/cfx/hair/houdini/z88100.cfx.hair.v010.hip')
 
 utl_gui_qt_core.set_window_show_standalone(
     utl_pnl_widgets.ShotRenderSubmitter, hook_option=hook_option
