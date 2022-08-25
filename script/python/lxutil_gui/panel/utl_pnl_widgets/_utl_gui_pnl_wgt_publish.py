05255f1c246a2e9502d14685c69328cd6a1bb09c
dongchangbao <dongchangbao@papegames.net>
2022-08-25 18:49:06 +0800

    common update

--- script/python/lxutil_gui/panel/utl_pnl_widgets/_utl_gui_pnl_wgt_publish.py
+++ script/python/lxutil_gui/panel/utl_pnl_widgets/_utl_gui_pnl_wgt_publish.py
@@ -4,16 +4,17 @@ from lxutil_gui.panel import utl_gui_pnl_abs_publish
 import lxutil.dcc.dcc_objects as utl_dcc_objects
 
 
-class DccValidatorOpt(utl_gui_pnl_abs_publish.AbsValidatorOpt):
+class ValidatorOpt(utl_gui_pnl_abs_publish.AbsValidatorOpt):
     DCC_NAMESPACE = 'katana'
     DCC_NODE_CLS = utl_dcc_objects.Obj
+    DCC_COMPONENT_CLS = utl_dcc_objects.Component
     DCC_SELECTION_CLS = None
     def __init__(self, *args, **kwargs):
-        super(DccValidatorOpt, self).__init__(*args, **kwargs)
+        super(ValidatorOpt, self).__init__(*args, **kwargs)
 
 
 class AssetPublish(utl_gui_pnl_abs_publish.AbsAssetPublish):
     DCC_NAMESPACE = 'python'
-    DCC_VALIDATOR_OPT_CLS = DccValidatorOpt
+    DCC_VALIDATOR_OPT_CLS = ValidatorOpt
     def __init__(self, session, *args, **kwargs):
         super(AssetPublish, self).__init__(session, *args, **kwargs)
